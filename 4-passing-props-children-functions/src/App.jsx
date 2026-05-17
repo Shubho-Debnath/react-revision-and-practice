0311e3ee720c1f048eae5bd1ff8f44c6ccbadf1c
Suvo <suvodevnath123@gmail.com>
2026-05-17 12:00:30 +0600

    feat: passing props, children, and function patterns

---
+++ 4-passing-props-children-functions/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import PassingPropsToChildren from './components/PassingPropsToChildren'
+
+function App() {
+
+  return (
+    <>
+    <PassingPropsToChildren />
+    </>
+  )
+}
+
+export default App
---
+++ 4-passing-props-children-functions/src/components/PassingPropsToChildren.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PassingPropsToChildren = () => {
+  return (
+    <div>
+      <h1>Passing Props To Children</h1>
+    </div>
+  )
+}
+
+export default PassingPropsToChildren
